20b31b5c8ae1b1cd8f28e8ad1f4e9fe3546c0710
Sourav2707 <smartsourav33@gmail.com>
2022-12-15 14:07:31 +0530

    UC2 displaying default values of primitive datatype

--- src/com/uc1_displaying_a_message/DisplayingAMessage.java
+++
@@ -1,7 +0,0 @@
-package com.uc1_displaying_a_message;
-
-public class DisplayingAMessage {
-    public static void main(String[] args) {
-        System.out.println("Displaying the message as a practice");
-    }
-}
