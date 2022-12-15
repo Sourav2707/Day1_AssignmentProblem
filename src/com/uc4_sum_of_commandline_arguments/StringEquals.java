bd3f6725b4ca6c3b88ed90c028c9d014016be7d2
Sourav2707 <smartsourav33@gmail.com>
2022-12-15 14:23:45 +0530

    UC4 summing the valid Integer commandline arguments

---
+++ src/com/uc4_sum_of_commandline_arguments/StringEquals.java
@@ -0,0 +1,19 @@
+package com.uc4_sum_of_commandline_arguments;
+
+public class StringEquals {
+    public static void main(String[] args)
+    {
+        int sum = 0;
+        int a = 0;
+        for(int i = 0; i < args.length; i++) {
+            try {
+                a = Integer.parseInt(args[i]);
+                sum+=a;
+            }
+            catch (Exception e){
+                System.out.println(args[i]+" is not a valid integer");
+            }
+        }
+        System.out.println("Sum of valid integer is "+sum);
+    }
+}
