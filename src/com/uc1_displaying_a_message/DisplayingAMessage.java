5fde06a4787674bdf87dee2d75618b89a3d0b163
Sourav2707 <smartsourav33@gmail.com>
2022-12-15 14:02:56 +0530

    UC1 displaying a message

--- src/com/uc1_displaying_a_message/DisplayingAMessage.java
+++ src/com/uc1_displaying_a_message/DisplayingAMessage.java
@@ -1,6 +1,6 @@
 package com.uc1_displaying_a_message;
 
-public class Main {
+public class DisplayingAMessage {
     public static void main(String[] args) {
         System.out.println("Displaying the message as a practice");
     }
