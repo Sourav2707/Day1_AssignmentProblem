a467b496ad9ada28c01df3c114b43e1f339e166e
Sourav2707 <smartsourav33@gmail.com>
2022-12-16 14:49:35 +0530

    UC7 checking whether the given year is leapyear

---
+++ src/com/uc1_displaying_a_message/DisplayingAMessage.java
@@ -0,0 +1,7 @@
+package com.uc1_displaying_a_message;
+
+public class DisplayingAMessage {
+    public static void main(String[] args) {
+        System.out.println("Displaying the message as a practice");
+    }
+}
---
+++ src/com/uc5_displaying_name/DisplayingName.java
@@ -0,0 +1,9 @@
+package com.uc5_displaying_name;
+
+public class DisplayingName {
+    public static void main(String[] args)
+    {
+        String name = args[0];
+        System.out.println("My name is "+name);
+    }
+}
